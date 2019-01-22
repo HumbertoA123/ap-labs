973c4125f6da217f578baf9179879c14bb67f098
HumbertoA123 <humberto_atondo@hotmail.com>
2019-01-22 16:05:32 -0600

    Add files via upload

---
+++ Labs/lab0.1/1-23.h
@@ -0,0 +1,14 @@
+//
+//  1-23.h
+//  C Practice
+//
+//  Created by Humberto Atondo on 1/21/19.
+//  Copyright © 2019 Humberto Atondo. All rights reserved.
+//
+
+#ifndef __23_h
+#define __23_h
+
+#include <stdio.h>
+
+#endif /* __23_h */
