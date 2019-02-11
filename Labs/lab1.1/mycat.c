456e2ce0a829ec02c2e5d877e83f7598e7f43083
Humberto Atondo <a00959979@itesm.mx>
2019-02-11 13:30:47 -0600

    lab1.1_update

--- Labs/lab1.1/mycat.c
+++ Labs/lab1.1/mycat.c
@@ -1,21 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-FILE *file;
+FILE *file, *fileO;
 int c;
 
 int main(int argc, char **argv)
 {
+
 	file = fopen(argv[1], "r");
 
-	if(file)
+	if(file == NULL)
+	{
+		fprintf(stderr, "No such file\n");
+      	exit(EXIT_FAILURE);
+	}
+
+	if (argc == 2)
 	{
 		while((c = getc(file)) != EOF)
 		{
 			printf("%c", c);
 		}
+		printf("\n");
 	}
 
-	printf("\n");
+	if(argc == 3)
+	{
+		fileO = fopen(argv[2], "w");
+		while((c = getc(file)) != EOF)
+		{
+			fprintf(fileO, "%c", c);
+		}
+	}
+
 	fclose(file);
 
 	return 0;
