bcd7d4bbe89d5d378a0f675a55f08a1819cfa8cb
Humberto Atondo <humberto_atondo@hotmail.com>
2019-03-12 22:40:39 -0600

    lab2.4 not done yet

---
+++ labs/lab2.4/logger.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <string.h>
+#include <syslog.h>
+#include <stdarg.h>
+
+
+const char* TAG_INFO = "INFO";
+const char* TAG_WARNING = "WARNING";
+const char* TAG_ERROR = "ERROR";
+const char* TAG_PANIC = "PANIC";
+
+int loggerMode = 0;
+
+int initLogger(char* logType)
+{
+	if(strcmp("stdout", logType))
+	{
+		loggerMode = 0;
+	}
+	else if(strcmp("syslog", logType))
+	{
+		loggerMode = 1;
+	}
+	else
+	{
+		printf("Invalid input\n");
+		return -1;
+	}
+	return 0;
+}
+
+
+int infof(const char* tag, const char* message)
+{
+	if(loggerMode == 0)
+	{
+		printf("\033[1;32m");
+		printf(" [%s]: %s\n", tag, message);
+		printf("\033[0m");
+	}
+	else
+	{
+		syslog(LOG_USER | LOG_EMERG, tag, message);
+		printf("%d", loggerMode);
+	}
+
+	return 0;
+}
+
+
+int warnf(const char* tag, const char* message)
+{
+	printf("\033[1;33m");
+	printf(" [%s]: %s\n", tag, message);
+	printf("\033[0m");
+
+	return 1;
+}
+
+
+int errorf(const char* tag, const char* message)
+{
+	printf("\033[1;31m");
+	printf(" [%s]: %s\n", tag, message);
+	printf("\033[0m");
+
+	return -1;
+}
+
+
+int panicf(const char* tag, const char* message)
+{
+	printf("\033[1;35m");
+	printf(" [%s]: %s\n", tag, message);
+	printf("\033[0m");
+
+	return -2;
+}
+
+/*
+int main()
+{
+	infof(TAG_INFO, "Hello Again");
+	warnf(TAG_WARNING, "Hello Again");
+	errorf(TAG_ERROR, "Hello Again");
+	panicf(TAG_PANIC, "Hello Again");
+
+	return 0;
+}
+*/
---
+++ labs/lab2.4/testLogger.c
@@ -0,0 +1,17 @@
+
+#include "logger.h"
+
+int main()
+{
+	initLogger("stdout");
+
+	infof(TAG_INFO, "This is an info log.");
+	warnf(TAG_WARNING, "This is a warning log.");
+	errorf(TAG_ERROR, "This is an error log.");
+	panicf(TAG_PANIC, "This is a panic log.");
+
+	initLogger("syslog");
+	infof(TAG_INFO, "THIS IS AN INFO LOG.");
+
+	return 0;
+}
