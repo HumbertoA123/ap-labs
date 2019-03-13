313f1876124775e9c3ed0063391789db533961d9
Humberto Atondo <humberto_atondo@hotmail.com>
2019-03-12 23:08:54 -0600

    lab2.4 makefile1.6

--- labs/lab2.4/logger.h
+++ labs/lab2.4/logger.h
@@ -1,8 +1,3 @@
-const char* TAG_INFO = "INFO";
-const char* TAG_WARNING = "WARNING";
-const char* TAG_ERROR = "ERROR";
-const char* TAG_PANIC = "PANIC";
-
 int initLogger(char* logType);
 int infof(const char* tag, const char* message);
 int warnf(const char* tag, const char* message);
--- labs/lab2.4/testLogger.c
+++ labs/lab2.4/testLogger.c
@@ -4,17 +4,17 @@
 int main()
 {
 	initLogger("stdout");
-	infof(TAG_INFO, "This is an info log.");
-	warnf(TAG_WARNING, "This is a warning log.");
-	errorf(TAG_ERROR, "This is an error log.");
-	panicf(TAG_PANIC, "This is a panic log.");
+	infof("Info", "This is an info log.");
+	warnf("Warning", "This is a warning log.");
+	errorf("Error", "This is an error log.");
+	panicf("Panic", "This is a panic log.");
 
 
 	initLogger("syslog");
-	infof(TAG_INFO, "THIS IS AN INFO LOG.");
-	warnf(TAG_WARNING, "This is a warning log.");
-	errorf(TAG_ERROR, "This is an error log.");
-	panicf(TAG_PANIC, "This is a panic log.");
+	infof("Info", "THIS IS AN INFO LOG.");
+	warnf("Warning", "This is a warning log.");
+	errorf("Error", "This is an error log.");
+	panicf("Panic", "This is a panic log.");
 
 	return 0;
 }
