a407932798a9446a4ad4a4213b27e9ae09980890
HumbertoA123 <humberto_atondo@hotmail.com>
2019-03-29 11:46:27 -0500

    Basic demo runnin, pid and status

--- challenges/chal3/mytop/mytop.c
+++ challenges/chal3/mytop/mytop.c
@@ -1,13 +1,83 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <dirent.h>
+#include <unistd.h>
 
+void print_status(long tgid);
 void clear();
+
+void print_status(long tgid) {
+  char path[40], line[100], *p;
+  char *pPid;
+  FILE* statusf;
+
+  snprintf(path, 40, "/proc/%ld/status", tgid);
+
+  statusf = fopen(path, "r");
+  if(!statusf)
+    return;
+
+  while(fgets(line, 100, statusf)) {
+    /*
+    if(strncmp(line, "PPid:", 5) != 0)
+      continue;
+    //Ignore "PPid:" and whitespace
+    pPid = line + 5;
+    while(isspace(*pPid)) ++pPid;
+    */
+    
+    if(strncmp(line, "State:", 6) == 0)
+    {
+      p = line + 7;
+      while(isspace(*p)) ++p;
+    }
+    else
+    {
+      continue;
+    }
+    // Ignore "State:" and whitespace
+    //p = line + 7;
+    //while(isspace(*p)) ++p;
+    
+
+    printf("%6ld %s", tgid, p);
+    //printf("%6s", pPid);
+    //break;
+  }
+  fclose(statusf);
+}
 
 int main(){
   // Place your magic here
-  clear();
-  return 0;
+  while(1){
+
+
+    DIR* proc = opendir("/proc");
+    struct dirent* ent;
+    long tgid;
+
+    if(proc == NULL) {
+        perror("opendir(/proc)");
+    return 1;
+    }
+
+    while((ent = readdir(proc))) {
+        if(!isdigit(*ent->d_name))
+            continue;
+
+        tgid = strtol(ent->d_name, NULL, 10);
+
+        print_status(tgid);
+    }
+    closedir(proc);
+    sleep(2);
+  }
+    
+    return 0;
 }
 
 void clear() {
-  printf("\e[1;1H\e[2J"); 
+    printf("\e[1;1H\e[2J"); 
 }
