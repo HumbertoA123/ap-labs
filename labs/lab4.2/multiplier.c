726adc11695bd7ac02213cc05a09e47486f58bf3
Humberto Atondo <humberto_atondo@hotmail.com>
2019-04-23 22:23:55 -0500

    lab4.2

--- labs/lab4.2/multiplier.c
+++ labs/lab4.2/multiplier.c
@@ -8,15 +8,6 @@
 #include "logger.h"
 #define _GNU_SOURCE
 
-long *readMatrix(char *filename);
-long *getCol(int col, long *matrix);
-long *getRow(int row, long *matrix);
-long *multiply(long *matA, long *matB);
-void *threadFunc(void *arg);
-int saveResultMatrix(long *result);
-int getLock(void);
-int releaseLock(int lock);
-long dotProduct(long *vec1, long *vec2);
 
 int NUM_BUFFERS;
 long **buffers;
@@ -24,11 +15,11 @@ pthread_mutex_t *mutexes;
 long *result;
 pthread_t threads[2000];
 
-struct DataStruct {
+struct Struct {
 	long *matA;
 	long *matB;
-	size_t rowPosition;
-	size_t colPosition;
+	size_t rowPos;
+	size_t colPos;
 	long *result;
 };
 
@@ -38,13 +29,17 @@ long *readMatrix(char *filename)
 	char *line = NULL;
 	size_t len = 0;
 	fp = fopen(filename, "r");
-	if (fp == NULL) {
+	if (fp == NULL)
+	{
 		return NULL;
 	}
+
 	long *matrix;
 	matrix = (long *)malloc(4000000 * sizeof(long));
+
 	int i = 0;
-	while (getline(&line, &len, fp) != -1) {
+	while (getline(&line, &len, fp) != -1)
+	{
 		matrix[i++] = strtol(line, NULL, 10);
 	}
 	free(line);
@@ -91,7 +86,8 @@ long *getRow(int row, long *matrix)
 
 int getLock(void)
 {
-	for (int i = 0; i < NUM_BUFFERS; i++) {
+	for (int i = 0; i < NUM_BUFFERS; i++)
+	{
 		if (pthread_mutex_trylock(&mutexes[i]) == 0)
 		{
 			return i;
@@ -113,24 +109,53 @@ long dotProduct(long *v1, long *v2)
 {
 	long result = 0;
 	for (int i = 0; i < 2000; i++)
+	{
 		result += v1[i] * v2[i];
+	}
 	return result;
+}
+
+void *threadFunc(void *arg)
+{
+	struct Struct *data = (struct Struct *)arg;
+	long index;
+	int lock1, lock2;
+
+	while ((lock1 = getLock()) == -1);
+	while ((lock2 = getLock()) == -1);
+
+	buffers[lock1] = getRow(data->rowPos, data->matA);
+	buffers[lock2] = getCol(data->colPos, data->matB);
+	
+	index = ((((data->rowPos - 1) * 2000) + data->colPos) - 1);
+	data->result[index] = dotProduct(buffers[lock1], buffers[lock2]);
+
+	free(buffers[lock1]);
+	free(buffers[lock2]);
+	free(arg);
+
+	while (releaseLock(lock1) != 0);
+	while (releaseLock(lock2) != 0);
+
+	return NULL;
 }
 
 long *multiply(long *matA, long *matB)
 {
 	result = (long *)malloc(4000000 * sizeof(long));
 
-	for (size_t i = 0; i < 2000; i++) {
-		for (size_t j = 0; j < 2000; j++) {
-			struct DataStruct *currentStruct;
+	for (size_t i = 0; i < 2000; i++)
+	{
+		for (size_t j = 0; j < 2000; j++)
+		{
+			struct Struct *currentStruct;
 			currentStruct =
-			    (struct DataStruct *)
-			    malloc(sizeof(struct DataStruct));
+			    (struct Struct *)
+			    malloc(sizeof(struct Struct));
 			currentStruct->matA = matA;
 			currentStruct->matB = matB;
-			currentStruct->rowPosition = i + 1;
-			currentStruct->colPosition = j + 1;
+			currentStruct->rowPos = i + 1;
+			currentStruct->colPos = j + 1;
 			currentStruct->result = result;
 			pthread_create(&threads[j], NULL, threadFunc,
 				       (void *)currentStruct);
@@ -147,48 +172,31 @@ long *multiply(long *matA, long *matB)
 int saveResultMatrix(long *result)
 {
 	FILE *f = fopen("result.dat", "w");
-	if (f == NULL) {
+	if (f == NULL)
+	{
 		errorf("error: couldn't open/create result.dat\n");
 		return -1;
 	}
 
 	for (size_t i = 0; i < 4000000; i++)
+	{
 		fprintf(f, "%ld\n", result[i]);
+	}
 
 	fclose(f);
 	return 0;
 }
 
-void *threadFunc(void *arg)
-{
-	struct DataStruct *data = (struct DataStruct *)arg;
-	long index;
-
-	int lock1, lock2;
-	while ((lock1 = getLock()) == -1);
-	while ((lock2 = getLock()) == -1);
-	buffers[lock1] = getRow(data->rowPosition, data->matA);
-	buffers[lock2] = getCol(data->colPosition, data->matB);
-	
-	index = ((((data->rowPosition - 1) * 2000) + data->colPosition) - 1);
-	data->result[index] = dotProduct(buffers[lock1], buffers[lock2]);
-
-	free(buffers[lock1]);
-	free(buffers[lock2]);
-	free(arg);
-	while (releaseLock(lock1) != 0);
-	while (releaseLock(lock2) != 0);
-	return NULL;
-}
-
 int main(int argc, char **argv)
 {
-	if (argc != 3) {
+	if (argc != 3)
+	{
 		errorf("Please enter the number of buffers.\n");
 		exit(EXIT_FAILURE);
 	}
 	NUM_BUFFERS = strtol(argv[2], NULL, 10);
-	if (NUM_BUFFERS < 10){
+	if (NUM_BUFFERS < 10)
+	{
 		errorf("For the proper functioning of the program please give at least 10 buffers.\n");
 		exit(EXIT_FAILURE);
 	}
@@ -196,7 +204,8 @@ int main(int argc, char **argv)
 	buffers = (long **)malloc(NUM_BUFFERS * sizeof(long *));
 	mutexes = (pthread_mutex_t *) malloc(NUM_BUFFERS * sizeof(pthread_mutex_t));
 
-	for (int i = 0; i < NUM_BUFFERS; i++) {
+	for (int i = 0; i < NUM_BUFFERS; i++)
+	{
 		pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 		mutexes[i] = mutex;
 		pthread_mutex_init(&mutexes[i], NULL);
