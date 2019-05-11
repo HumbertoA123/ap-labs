f4798f046725412edde97b7674de12db64096f99
Humberto Atondo <humberto_atondo@hotmail.com>
2019-05-11 14:40:47 -0500

    multiple balls

--- challenges/chal4/mt_rainning_balls.go
+++ challenges/chal4/mt_rainning_balls.go
@@ -11,26 +11,32 @@ const (
 	PrintColor = "\033[38;5;%dm%s\033[39;49m\n"
 	startingVal = 88
 	maxHeight = 10
-	n = 40
-	m = 40
+	n = 20
+	m = 20
 )
 
 /*Global Variables*/
-var voxelMap [][]int
+var voxelMap [][][]int
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 	voxelMap = generateVoxelMap()
 	displayVoxelMap()
-	rain()
+	runRain()
 }
 
-func generateVoxelMap() [][]int {
-	newVoxelMap := [][]int{}
+func generateVoxelMap() [][][]int {
+	newVoxelMap := [][][]int{}
 	for i := 0; i < n; i++ {
-		row := []int{}
+		row := [][]int{}
 		for j := 0; j < m; j++ {
-			row = append(row, rand.Intn(maxHeight))
+			if i == 0 || i == n - 1 || j == 0 || j == m - 1 {
+				s := []int{0, 0}
+				row = append(row, s)
+			} else {
+				s := []int{rand.Intn(maxHeight), 0}
+				row = append(row, s)
+			}
 		}
 		newVoxelMap = append(newVoxelMap, row)
 	}
@@ -40,54 +46,47 @@ func generateVoxelMap() [][]int {
 func displayVoxelMap() {
 	for i := 0; i < len(voxelMap); i++ {
 		for j := 0; j < len(voxelMap); j++ {
-			fmt.Printf("\033[%d;%dH", i + 2, (j * 3) + 2)
-			fmt.Printf(PrintColor, startingVal + (5 - (voxelMap[i][j]) % 5), "*")
+			fmt.Printf("\033[%d;%dH", i  + 2, (j * 3) + 2)
+			if voxelMap[i][j][1] == 0 {
+				fmt.Printf(PrintColor, startingVal + (5 - (voxelMap[i][j][0]) % 5), "*")
+			} else {
+				fmt.Printf(PrintColor, 3, "o")
+			}
 		}
+	}
+}
+
+
+func runRain() {
+	for i := 0; i < 10; i++ {
+		rain()
 	}
 }
 
 func rain() {
 	x := rand.Intn(n)
 	y := rand.Intn(m)
+	voxelMap[x][y][1] = 1
 
 	for true {
-
-		for i := 0; i < n; i++ {
-			for j := 0; j < m; j++ {
-				if i == x && j == y {
-					fmt.Printf("\033[%d;%dH", x + 2, (y * 3) + 2)
-					fmt.Printf(PrintColor, 4, "o")
-				}else {
-					fmt.Printf("\033[%d;%dH", i + 2, (j * 3) + 2)
-					fmt.Printf(PrintColor, startingVal + (5 - (voxelMap[i][j]) % 5), "*")
-				}
-			}
-		}
-
-		if (y + 1) <= (m - 1) && voxelMap[x][y + 1] < voxelMap[x][y] {
+		displayVoxelMap()
+		time.Sleep(time.Millisecond * 2000)
+		if y < (m - 1) && voxelMap[x][y + 1][0] < voxelMap[x][y][0] && voxelMap[x][y + 1][1] == 0 {
+			voxelMap[x][y][1] = 0
 			y += 1
-		} else if (y + 1) <= (m - 1) && (x + 1) <= (n - 1) && voxelMap[x + 1][y + 1] < voxelMap[x][y] {
+		} else if x < (n - 1) && voxelMap[x + 1][y][0] < voxelMap[x][y][0] && voxelMap[x + 1][y][1] == 0 {
+			voxelMap[x][y][1] = 0
 			x += 1
-			y += 1
-		} else if (x + 1) <= (n - 1) && voxelMap[x + 1][y] < voxelMap[x][y] {
-			x += 1
-		} else if (y - 1) >= 0 && (x + 1) <= (n - 1) && voxelMap[x + 1][y - 1] < voxelMap[x][y] {
-			x += 1
+		} else if y > 0 && voxelMap[x][y - 1][0] < voxelMap[x][y][0] && voxelMap[x][y - 1][1] == 0 {
+			voxelMap[x][y][1] = 0
 			y -= 1
-		} else if (y - 1) >= 0 && voxelMap[x][y -1] < voxelMap[x][y] {
-			y -= 1
-		} else if (y - 1) >= 0 && (x - 1) >= 0 && voxelMap[x - 1][y - 1] < voxelMap[x][y] {
+		} else if x > 0 && voxelMap[x - 1][y][0] < voxelMap[x][y][0] && voxelMap[x - 1][y][1] == 0 {
+			voxelMap[x][y][1] = 0
 			x -= 1
-			y -= 1
-		} else if (x - 1) >= 0 && voxelMap[x - 1][y] < voxelMap[x][y] {
-			x -= 1
-		} else if (y + 1) <= (m - 1) && (x + 1) >= 0 && voxelMap[x - 1][y + 1] < voxelMap[x][y] {
-			x -= 1
-			y += 1
 		} else {
 			break
 		}
-		time.Sleep(3000 * time.Millisecond)
+		voxelMap[x][y][1] = 1
 	}
 }
 
